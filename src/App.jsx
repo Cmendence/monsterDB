00b7e4f9a7da2f95e706d185cb440db21e268138
Cmendence <chris.mendence@gmail.com>
2024-01-22 10:18:17 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='text-2xl text-red-500 font-extrabold'>TESTING</div>
+    </>
+  )
+}
+
+export default App
