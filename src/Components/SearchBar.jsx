2e6146edcfda9e4a789a2c1d640e7f4c9c198300
Cmendence <chris.mendence@gmail.com>
2024-04-03 12:10:23 -0400

    right padding to search bar to prevent typing over button

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -1,28 +1,21 @@
+export default function SearchBar({ query, setQuery, setCurrentPage }) {
+  const handleSearch = () => {
+    setCurrentPage(1);
+  };
 
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter" || e.key === 13) {
+      // Close the keyboard by blurring the input element
+      e.target.blur();
 
-export default function SearchBar({
-  query,
-  setQuery,
-  setCurrentPage,
-}) {
+      handleSearch();
+    }
+  };
 
-   const handleSearch = () => {
-      setCurrentPage(1);
-    };
-  
-    const handleKeyPress = (e) => {
-     if (e.key === 'Enter' || e.key === 13) {
-       // Close the keyboard by blurring the input element
-       e.target.blur();
-  
-       handleSearch()
-     }
-   };
-
-   const clearSearch = (e) => {
-      setQuery("");
-      setCurrentPage(1);
-    };
+  const clearSearch = (e) => {
+    setQuery("");
+    setCurrentPage(1);
+  };
 
   const magnifyingGlass = (
     <svg
@@ -61,32 +54,32 @@ export default function SearchBar({
   return (
     <div className=" flex justify-center my-2">
       <div className="relative w-[215px]">
-      <button
-        className="text-violet-700 focus:outline-none absolute top-3 -left-5"
-        onClick={handleSearch}
-        type="button"
-      >
-        {magnifyingGlass}
-      </button>
-      <input
-        type="text"
-        placeholder="Search Monsters"
-        className="w-48 p-1 border-2  border-violet-500 rounded-lg text-sm m-2 focus:border-violet-700 focus:outline-none text-gray-600 "
-        name="search"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-        onKeyDown={handleKeyPress}
-      />
-      { query && (
-      <button
-        className={`text-violet-700 focus:outline-none absolute right-5 top-3`}
-        onClick={clearSearch}
-        type="button"
-        onPointerDown={(e) => e.preventDefault()}
-      >
-        {xIcon}
-      </button>
-)}     
+        <button
+          className="text-violet-700 focus:outline-none absolute top-3 -left-5"
+          onClick={handleSearch}
+          type="button"
+        >
+          {magnifyingGlass}
+        </button>
+        <input
+          type="text"
+          placeholder="Search Monsters"
+          className="w-48 p-1 pr-6 border-2  border-violet-500 rounded-lg text-sm m-2 focus:border-violet-700 focus:outline-none text-gray-600 "
+          name="search"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          onKeyDown={handleKeyPress}
+        />
+        {query && (
+          <button
+            className={`text-violet-700 focus:outline-none absolute right-5 top-3`}
+            onClick={clearSearch}
+            type="button"
+            onPointerDown={(e) => e.preventDefault()}
+          >
+            {xIcon}
+          </button>
+        )}
       </div>
     </div>
   );
